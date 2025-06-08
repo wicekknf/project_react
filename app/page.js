c6a24b73c5a83bfea71b9ea37a35f53dd7d7d037
Wiktor Łaziński <lazinski.w@wp.pl>
2025-06-08 14:42:13 +0200

    done

--- app/page.js
+++ app/page.js
@@ -1,103 +1,169 @@
 import Image from "next/image";
 
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <nav>
+        <div className="logo-nav">
+          
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <div className="nav-list">
+          <a className="a-style" href="#">Home</a>
+          <a className="a-style" href="#about-us">O nas</a>
+          <a className="a-style" href="#offer">Oferta</a>
+          
+        </div>
+
+        <div className="mini-nav">
+          <div className="icon-box">
+            <div className="hamburger-icon">
+              <i className="ti ti-menu-2"></i>
+            </div>
+            <div className="close-icon">
+              <i className="ti ti-x"></i>
+            </div>
+          </div>
+          <div className="mini-nav-list">
+            <a href="#">Home</a>
+            <a href="#about-us">O nas</a>
+            <a href="#offer">Oferta</a>
+            
+          </div>
+        </div>
+      </nav>
+
+      <div className="hero">
+        <div className="hero-shadow"></div>
+        <div className="hero-text">
+          <p>Forest Group</p>
+          <h1>Najpiękniejszy park w Twojej okolicy!</h1>
+        </div>
+      </div>
+
+      <section id="about-us" className="about-us wrapper">
+        <h2 className="section-title">o nas</h2>
+        <div className="about-us-box">
+          <div className="about-us-img">
+            <img
+              src="/img/waters-big.jpg"
+            
+              alt="Mała wysepka na środku jeziora. Wszędzie bujna roślinność a na wysepce jedno małe drzewko"
+            />
+          </div>
+          <div className="about-us-text">
+            <h3>Niezapomniane Przygody</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+              <a
+                href="http://www.MMCSchool.pl"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                www.MMCSchool.pl
+              </a>
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <div className="hero2">
+        <div className="hero2-shadow"></div>
+        <div className="hero2-text">
+          <p>Przygoda dla całej rodziny</p>
+        </div>
+      </div>
+
+      <section id="offer" className="wrapper offer">
+        <h2 className="section-title">oferta</h2>
+        <p className="info">Wybierz jedną z naszych wspaniałych ofert!</p>
+        <div className="cards">
+          <div className="card one">
+            <h3>Demo</h3>
+            <p className="price">$0</p>
+            <ul className="card-list">
+              <li><i className="ti ti-check"></i> opcja w pakiecie</li>
+              <li><i className="ti ti-check"></i> opcja w pakiecie</li>
+              <li><i className="ti ti-minus"></i> opcja poza pakietem</li>
+              <li><i className="ti ti-minus"></i> opcja poza pakietem</li>
+              <li><i className="ti ti-minus"></i> opcja poza pakietem</li>
+            </ul>
+            <button className="chose">Wybierz</button>
+          </div>
+
+          <div className="card two">
+            <h3>Standard</h3>
+            <p className="price">$99</p>
+            <ul className="card-list">
+              <li><i className="ti ti-check"></i> opcja w pakiecie</li>
+              <li><i className="ti ti-check"></i> opcja w pakiecie</li>
+              <li><i className="ti ti-check"></i> opcja w pakiecie</li>
+              <li><i className="ti ti-minus"></i> opcja poza pakietem</li>
+              <li><i className="ti ti-minus"></i> opcja poza pakietem</li>
+            </ul>
+            <button className="chose standard-btn">Wybierz</button>
+          </div>
+
+          <div className="card three">
+            <h3>Premium</h3>
+            <p className="price">$299</p>
+            <ul className="card-list">
+              <li><i className="ti ti-check"></i> opcja w pakiecie</li>
+              <li><i className="ti ti-check"></i> opcja w pakiecie</li>
+              <li><i className="ti ti-check"></i> opcja w pakiecie</li>
+              <li><i className="ti ti-check"></i> opcja w pakiecie</li>
+              <li><i className="ti ti-check"></i> opcja w pakiecie</li>
+            </ul>
+            <button className="chose">Wybierz</button>
+          </div>
+        </div>
+
+        <div className="card other">
+          <h3>Wszyskie oferty</h3>
+          <p className="price">Sprawdź nasze pozostałe oferty!</p>
+          <button className="chose">
+            <a href="offer.html">Sprawdź</a>
+          </button>
+        </div>
+      </section>
+
+      <footer>
+        <div className="footer-top">
+          <div className="footer-logo">
+            <div className="footer-logo-element">
+              <i className="ti ti-christmas-tree"></i> Forest
+            </div>
+          </div>
+          <div className="footer-info">
+            <div className="footer-info-boxes">
+              <h3>O nas</h3>
+              <a href="#">Historia</a>
+              <a href="#">Parki</a>
+              <a href="#">Opinie</a>
+            </div>
+            <div className="footer-info-boxes">
+              <h3>Nasza firma</h3>
+              <a href="#">Praca</a>
+              <a href="#">Kadry</a>
+              <a href="#">Zarząd</a>
+            </div>
+            <div className="footer-info-boxes">
+              <h3>Dokumenty</h3>
+              <a href="#">Wnioski</a>
+              <a href="#">Rezerwacje</a>
+            </div>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <p>&copy;<span className="year"></span> Forest Group Company Ltd.</p>
+        </div>
       </footer>
-    </div>
+    </>
   );
 }
